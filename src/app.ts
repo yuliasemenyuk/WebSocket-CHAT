2ee8e64decd60a895b6927776387773b75e70563
yuliasemenyuk <semenyukyp@gmail.com>
2024-10-06 16:49:39 +0300

    add cors options

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, ServerOptions } from "socket.io";
 import mongoose from "mongoose";
 import { setupSocketHandlers } from "./controllers/socketHandler";
 
 dotenv.config();
 
-const { PORT, DB_URL } = process.env;
+const { PORT, DB_URL, FE_URL } = process.env;
 
 if (!DB_URL) {
   console.error("DB_URL is not defined in the environment variables");
   process.exit(1);
 }
 
+const corsOptions: cors.CorsOptions = {
+  origin: FE_URL,
+  methods: ["GET", "POST"],
+  // allowedHeaders: ["my-custom-header"],
+  credentials: true,
+};
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: corsOptions,
+} as Partial<ServerOptions>);
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose.set("strictQuery", true);
-mongoose.connect(DB_URL)
+mongoose
+  .connect(DB_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
@@ -30,7 +42,7 @@ mongoose.connect(DB_URL)
 
 setupSocketHandlers(io);
 
-const port = PORT || 3000;
+const port = PORT || 5000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
